b1375d88229df18db6b6816b98bb464329b5d4e7
AntoineMartet <antmartet@gmail.com>
2023-11-26 01:31:31 +0100

    First version of tree. Added 2 lights

--- vite-project-three-js-pine-tree/main.js
+++ vite-project-three-js-pine-tree/main.js
@@ -23,7 +23,7 @@ const camera = new THREE.PerspectiveCamera(
     0.1,
     100
 )
-camera.position.z = 20
+camera.position.z = 40
 scene.add(camera)
 
 //Renderer
@@ -49,9 +49,17 @@ window.addEventListener('resize', () => {
 ----------------------------------------------------------------------*/
 
 //Light
-const light = new THREE.PointLight(0xffffff, 200, 100)
-light.position.set(0, -10, 10) // droite, haut, vers nous
-scene.add(light)
+const lightAbove = new THREE.PointLight(0xffffff, 400, 100, 2)
+lightAbove.position.set(0, 10, 3) // droite, haut, vers nous
+scene.add(lightAbove)
+
+const lightBelow = new THREE.PointLight(0xffffff, 400, 100, 2.5)
+lightBelow.position.set(0, -20, 3) // droite, haut, vers nous
+scene.add(lightBelow)
+
+const lightBelow2 = new THREE.PointLight(0xffffff, 300, 100, 2.5)
+lightBelow2.position.set(0, -20, -3) // droite, haut, vers nous
+scene.add(lightBelow2)
 
 //Controls
 const controls = new OrbitControls(camera, canvas)
@@ -60,7 +68,7 @@ controls.enablePan = false // right click to translate the scene
 controls.enableZoom = false // wheel to zoom in and out the scene
 controls.enableRotate = true // left click to rotate the scene
 controls.autoRotate = true
-controls.autoRotateSpeed = 2
+controls.autoRotateSpeed = 4
 
 //Update canvas regularly
 const loop = () => {
@@ -74,20 +82,46 @@ loop()
 ----------------------------------------------------------------------*/
 
 //Create our sphere
-const coneGeometry = new THREE.ConeGeometry(3, 6, 64, 8)
-const material = new THREE.MeshStandardMaterial({
+const cone1 = new THREE.ConeGeometry(3, 6, 64, 8)
+const material1 = new THREE.MeshStandardMaterial({
     color: "#00ff83",
     roughness: 0.5
 })
-const mesh = new THREE.Mesh(coneGeometry, material)
-scene.add(mesh)
+const mesh1 = new THREE.Mesh(cone1, material1)
+scene.add(mesh1)
+
+const cone2 = new THREE.ConeGeometry(4, 8, 64, 8)
+const mesh2 = new THREE.Mesh(cone2, material1)
+mesh2.position.set(0, -2, 0)
+scene.add(mesh2)
+
+const cone3 = new THREE.ConeGeometry(5, 10, 64, 8)
+const mesh3 = new THREE.Mesh(cone3, material1)
+mesh3.position.set(0, -4, 0)
+scene.add(mesh3)
+
+const cylinder1 =  new THREE.CylinderGeometry( 1, 1, 4, 32);
+const material2 = new THREE.MeshStandardMaterial({
+    color: "#cc5500",
+    roughness: 0.5
+})
+const mesh4 = new THREE.Mesh(cylinder1, material2)
+mesh4.position.set(0, -10, 0)
+scene.add(mesh4)
+
+function createPineTree(nb) {
+
+}
 
 /*----------------------------------------------------------------------
                        Scene - Animations
 ----------------------------------------------------------------------*/
 
 //gsap timeline, for a series of multiple animations
-const tl = gsap.timeline({defaults: { duration: 1}})
-tl.fromTo(mesh.scale, {x:0, y:0, z:0}, {x:1.0, y:1, z:1})
+const tl = gsap.timeline({defaults: { duration: 0.5}})
+tl.fromTo(mesh4.scale, {x:0, y:0, z:0}, {x:1.0, y:1, z:1})
+tl.fromTo(mesh3.scale, {x:0, y:0, z:0}, {x:1.0, y:1, z:1})
+tl.fromTo(mesh2.scale, {x:0, y:0, z:0}, {x:1.0, y:1, z:1})
+tl.fromTo(mesh1.scale, {x:0, y:0, z:0}, {x:1.0, y:1, z:1})
 tl.fromTo("nav", {y: "-100%"}, {y: "0%"})
 tl.fromTo(".title", {opacity:0, rotation:0}, {opacity:1, rotation:720})
